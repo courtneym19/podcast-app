19553e70f36314ed60fbdb3123f6d3e210583cd9
Courtney Miller <courtneymiller919@gmail.com>
2017-10-30 14:27:50 -0600

    sound issue persists

--- app/assets/javascripts/services/Episode.js
+++ app/assets/javascripts/services/Episode.js
@@ -6,7 +6,7 @@ app.factory('Episode', [ function(){
   var episodes = [
     { title: 'Another Title', number: '3', date: '8/15/17', description: 'Description #3', audioUrl: '/assets/sounds/ep1'},
     { title: 'Some Other Title', number: '2', date: '8/8/17', description: 'Best episode ever', audioUrl: '/assets/sounds/ep2'},
-    { title: 'Pilot Episode', number: '1', date: '8/1/17', description: 'Soul Spectrum pilot episode', audioUrl: 'https://drive.google.com/uc?export=download&id=0B76fdfZeVDaBQ25VZ2loNkJBd1k'}
+    { title: 'Pilot Episode', number: '1', date: '8/1/17', description: 'Soul Spectrum pilot episode', audioUrl: 'https://archive.org/download/soulspectrumpodcast_gmail_Ep1/ep1.mp3'}
   ];
 
   Episode.getEpisodes = function(){
